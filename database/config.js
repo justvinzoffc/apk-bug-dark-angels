6685f18295144b10275873a84949dea6c6c33117
justvinzoffc <justvinzofficial@gmail.com>
2025-11-15 12:47:47 +0700

    Create config.js

---
+++ database/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  tokens: "8156232618:AAGblU_57w9eUswvNcOh-_5wfO3QnZ-ZUtg",  // Ubah Jadi Token Bot Mu !!!
+  owner: "6696150439", // Ubah Jadi Id Mu !!!
+  port: "8082", // Ubah Jadi Port Panel Mu !!!
+  ipvps: "https://musang.servercloud.my.id" // Ubah Jadi Ip Vps Mu !!!
+};
